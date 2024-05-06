940790fe334787166a3e6c3454d820d8bee0cd7c
Lukas Petersson <lukas.petersson.1999@gmail.com>
2024-05-06 13:32:26 -0700

    runfile

--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ if [ ! -d "./logs/LongForecasting" ]; then
     mkdir ./logs/LongForecasting
 fi
 seq_len=96
+pred_len=96
+label_len=48
+
 model_name=PatchTST
 
 root_path_name=./dataset/
@@ -14,30 +17,28 @@ model_id_name=ETTh1
 data_name=ETTh1
 
 random_seed=2021
-for pred_len in 96
-do
-    python -u run_longExp.py \
-      --random_seed $random_seed \
-      --is_training 1 \
-      --root_path $root_path_name \
-      --data_path $data_path_name \
-      --model_id $model_id_name_$seq_len'_'$pred_len \
-      --model $model_name \
-      --data $data_name \
-      --features M \
-      --seq_len $seq_len \
-      --pred_len $pred_len \
-      --enc_in 7 \
-      --e_layers 3 \
-      --n_heads 4 \
-      --d_model 16 \
-      --d_ff 128 \
-      --dropout 0.3\
-      --fc_dropout 0.3\
-      --head_dropout 0\
-      --patch_len 16\
-      --stride 8\
-      --des 'Exp' \
-      --train_epochs 10\
-      --itr 1 --batch_size 557 --learning_rate 0.003 
-done
+python -u run_longExp.py \
+  --random_seed $random_seed \
+  --is_training 1 \
+  --root_path $root_path_name \
+  --data_path $data_path_name \
+  --model_id $model_id_name_$seq_len'_'$pred_len \
+  --model $model_name \
+  --data $data_name \
+  --features M \
+  --seq_len $seq_len \
+  --pred_len $pred_len \
+  --label_len $label_len \
+  --enc_in 7 \
+  --e_layers 3 \
+  --n_heads 4 \
+  --d_model 16 \
+  --d_ff 128 \
+  --dropout 0.3\
+  --fc_dropout 0.3\
+  --head_dropout 0\
+  --patch_len 16\
+  --stride 8\
+  --des 'Exp' \
+  --train_epochs 10\
+  --itr 1 --batch_size 557 --learning_rate 0.003 
